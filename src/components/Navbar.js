3048a2016144ff2fc1d4cd34f166fe3296fba764
Veselin <vesselin60@gmail.com>
2021-12-15 17:03:28 +0200

    Navbar and Styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,13 @@
 const Navbar = () => {
     return (
-        <h1>Navbar Almost before we knew it, we had left the ground.</h1>
+        <div className="navbar">
+            <div className="logo">
+                <img className="logo-img" src="./react-logo.png" alt="logo" />
+                <h3 className="logo-text">ReactFacts</h3>
+            </div>
+            <h4 className="title">React Course - Project 1</h4>
+        </div>
     );
-}
+};
 
 export default Navbar;
