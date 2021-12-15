1dd590873bead0a37e33f03ff5cfa2bc6a4a5268
Veselin <vesselin60@gmail.com>
2021-12-15 13:02:17 +0200

    initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,7 @@
+const Main = () => {
+    return (
+        <h1>Main Almost before we knew it, we had left the ground.</h1>
+    );
+}
+
+export default Main;
---
+++ src/components/Navbar.js
@@ -0,0 +1,7 @@
+const Navbar = () => {
+    return (
+        <h1>Navbar Almost before we knew it, we had left the ground.</h1>
+    );
+}
+
+export default Navbar;
